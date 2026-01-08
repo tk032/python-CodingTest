1dbb7854e9c83fb67d6ae6214e39136870a80800
tk032 <84540201+tk032@users.noreply.github.com>
2026-01-08 19:23:48 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,9 @@
+def solution(a, b, c):
+    if a != b and a != c and b != c:
+        answer = a + b + c
+    elif a == b and b == c and a == c:
+        answer = (a + b + c) * ( a**2 + b**2 + c**2)* (a**3 + b**3 + c**3)
+    else:
+        answer = (a+b+c) * ( a**2 + b**2 + c**2)
+    return answer
+
